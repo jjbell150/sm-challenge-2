c0e58897d8ddabd5271f9fbf919d2c8ef641ab9b
Jesse Bell <jjbell150@gmail.com>
2023-10-16 13:48:52 +1100

    Run linter

--- index.js
+++ index.js
@@ -15,13 +15,8 @@ const TIME_MAP = {
 
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
-
   const [hours, minutes] = time.split(':');
 
-  let prefix;
-  let suffix;
-
-  // TODO: real code goes here!
   if (hours === '0') {
     return 'midnight';
   } else if (hours === '12') {
@@ -29,13 +24,12 @@ function convertTimeToWords(time) {
   }
 
   if (minutes === '0') {
-    return `${TIME_MAP[hours]} o\'clock`
+    return `${TIME_MAP[hours]} o\'clock`;
   } else if (minutes === '15') {
     return `quarter past ${TIME_MAP[hours]}`;
   } else if (minutes === '30') {
     return `half past ${TIME_MAP[hours]}`;
   } else if (minutes === '45') {
-    console.log(hours)
     return `quarter to ${TIME_MAP[Number(hours) + 1]}`;
   }
 
@@ -46,10 +40,9 @@ function convertTimeToWords(time) {
     if (Number(firstDigit) > 30) {
       return `${TIME_MAP[firstDigit]} ${TIME_MAP[secondDigit]} to ${TIME_MAP[Number(hours) + 1]}`;
     } else {
-      return `${TIME_MAP[firstDigit]} ${TIME_MAP[secondDigit]} past ${TIME_MAP[Number(hours)]}`;
+      return `${TIME_MAP[firstDigit]} ${TIME_MAP[secondDigit]} past ${TIME_MAP[hours]}`;
     }
   }
 }
-
 
 module.exports = { convertTimeToWords };
